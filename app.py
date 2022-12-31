0f274545ffb10959a53b8393157aa41e437531c9
data-science-institute <114200091+data-science-institute@users.noreply.github.com>
2022-12-31 13:48:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
-import time
+import torch
+from transformers import pipeline
 
-latest_iteration = st.empty()
-bar = st.progress(0)
+st.title("英語文章要約AI")
 
-for i in range(100):
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i+1) # 0〜1か0〜100
-    time.sleep(1)
+text = st.text_area("英語の文章を入力してください", placeholder="Write Here")
+
+if st.button("要約"):
+    "\n"
+    with st.spinner("要約中..."):
+        pipe = pipeline("summarization")
+        result = pipe(text, max_length=130, min_length=30)
+    container = st.container()
+    container.write("要約結果")
+    st.success(result[0]["summary_text"])
